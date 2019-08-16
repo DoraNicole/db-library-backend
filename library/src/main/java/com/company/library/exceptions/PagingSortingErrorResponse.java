d238379c54b616daff21fc8a5b9d9770aa504cf9
Brebu <brebu.ciprian@gmail.com>
2019-08-16 15:42:11 +0300

    Upload image feature

---
+++ library/src/main/java/com/company/library/exceptions/PagingSortingErrorResponse.java
@@ -0,0 +1,21 @@
+package com.company.library.exceptions;
+public class PagingSortingErrorResponse {
+private int errorCode;
+private String message;
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- library/src/main/java/com/company/library/service/BookService.java
+++ library/src/main/java/com/company/library/service/BookService.java
@@ -3,6 +3,10 @@ package com.company.library.service;
 import com.company.library.model.Book;
 import com.company.library.repository.BookRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -36,5 +40,19 @@ public class BookService implements BookServiceInterface {
         Predicate<Book> searchByGenre = b -> b.getGenre().toLowerCase().contains(query.toLowerCase());
         Predicate<Book> searchByAuthor = b -> b.getAuthor().toLowerCase().contains(query.toLowerCase());
         return bookRepositoryInterface.findAll().stream().filter(searchByTitle.or(searchByGenre).or(searchByAuthor)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Page<Book> findPaginatedBooks(String orderBy, String direction, int page, int size) {
+        Sort sort = null;
+        if (direction.equals("ASC")) {
+            sort = new Sort(new Sort.Order(Sort.Direction.ASC, orderBy));
+        }
+        if (direction.equals("DESC")) {
+            sort = new Sort(new Sort.Order(Sort.Direction.DESC, orderBy));
+        }
+        Pageable pageable = new PageRequest(page, size, sort);
+        Page<Book> data = bookRepositoryInterface.findAll(pageable);
+        return data;
     }
 }
