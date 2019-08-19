5ef471464a94f9d06e030a62aa23d5f9ccfa4672
Andrei <andreirotaru1998@gmail.com>
2019-08-19 14:13:42 +0300

    simple penalties are working

---
+++ library/src/main/java/com/company/library/exceptions/UserHasPenaltiesException.java
@@ -0,0 +1,8 @@
+package com.company.library.exceptions;
+
+public class UserHasPenaltiesException extends Exception {
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
