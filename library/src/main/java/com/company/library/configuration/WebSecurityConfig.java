7c5accd1be0b955b7e4e5d77e14eef1905e4e02a
Brebu <brebu.ciprian@gmail.com>
2019-08-21 15:07:52 +0300

    CORS implemented

--- library/src/main/java/com/company/library/configuration/WebSecurityConfig.java
+++ library/src/main/java/com/company/library/configuration/WebSecurityConfig.java
@@ -17,6 +17,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -34,15 +43,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // configure AuthenticationManager so that it knows from where to load
-        // user for matching credentials
-        // Use BCryptPasswordEncoder
+
         auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        List<String> stringList1 = Arrays.asList("*");
+        configuration.setAllowedOrigins(Collections.unmodifiableList(stringList1));
+        List<String> stringList2 = Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH");
+        configuration.setAllowedMethods(Collections.unmodifiableList(stringList2));
+
+        configuration.setAllowCredentials(true);
+
+        List<String> stringList = Arrays.asList("Authorization", "Cache-Control", "Content-Type");
+        configuration.setAllowedHeaders(Collections.unmodifiableList(stringList));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
@@ -53,20 +79,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        // We don't need CSRF for this example
-//        httpSecurity.csrf().disable()
-//                // dont authenticate this particular request
-//                .authorizeRequests()
-//                .antMatchers("/authenticate", "/register", "/allusers").permitAll().
-//                // all other requests need to be authenticated
-//                        anyRequest().authenticated().and().
-//                // make sure we use stateless session; session won't be used to
-//                // store user's state.
-//                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        httpSecurity.cors().disable();
+
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate", "/register", "/registerConfirm", "/allusers", "/forgotpassword", "/resetpassword").permitAll().
+                anyRequest().authenticated().and().
+                exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.cors();
         httpSecurity.csrf().disable();
+
+
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
     }
 
