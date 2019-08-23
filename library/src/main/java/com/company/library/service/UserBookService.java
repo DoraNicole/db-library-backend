2527131d0e393150a6b285a6d86c7e98a5abc44d
Andrei <andreirotaru1998@gmail.com>
2019-08-23 10:02:20 +0300

    commit

--- library/src/main/java/com/company/library/service/UserBookService.java
+++ library/src/main/java/com/company/library/service/UserBookService.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @Service
 public class UserBookService implements UserBookServiceInterface {
+
     @Autowired
     private UserBookRepositoryInterface userBookRepositoryInterface;
 
