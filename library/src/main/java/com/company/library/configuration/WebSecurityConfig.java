fc38f894be979fdba6034c556c2a78a8323a1208
Brebu <brebu.ciprian@gmail.com>
2019-09-11 11:11:52 +0300

    OutOfSTockAndCropImage

--- library/src/main/java/com/company/library/configuration/WebSecurityConfig.java
+++ library/src/main/java/com/company/library/configuration/WebSecurityConfig.java
@@ -87,7 +87,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // dont authenticate this particular request
                 .authorizeRequests()
                 .antMatchers("/authenticate", "/register", "/registerConfirm",
-                        "/reminder", "/forgotpassword", "/resetpassword","/returnBook", "/resendVerificationLink").permitAll().
+                        "/reminder", "/forgotPassword", "/resetPassword","/returnBook", "/resendVerificationLink").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
